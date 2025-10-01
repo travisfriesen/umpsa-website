25e37608113717d5fe63d53343c3c91ff7ce71ea
travisfriesen <github@travisfriesen.ca>
2025-10-01 17:40:30 -0500

    feat: header

---
+++ src/components/header.tsx
@@ -0,0 +1,22 @@
+"use client";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+
+export const Header = () => {
+    return (
+        <div className={`bg-green-200 w-full h-30 grid items-center justify-center grid-cols-4`}>
+            <div className={`flex justify-center items-center col-start-1 col-end-2 max-h-40`}>
+                <Image
+                    src={`svg/logo-notext.svg`}
+                    alt={`logo`}
+                    width={125}
+                    height={125}
+                />
+            </div>
+            <div className={`text-gold-950 text-xl flex flex-row gap-6 col-start-2 col-end-4 justify-center items-center `}>
+                <h1>Test</h1>
+                <h1>Test2</h1>
+            </div>
+        </div>
+    )
+}
