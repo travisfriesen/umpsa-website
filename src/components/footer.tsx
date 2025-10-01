daffcb491545912c8c3adf04f4fea91024026460
travisfriesen <github@travisfriesen.ca>
2025-10-01 17:40:54 -0500

    feat: footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+    return (
+        <div className={`bg-green-200 w-full h-30 text-gold-950`}>
+            <div className={`flex justify-center items-center`}>
+                <p>
+                    © University of Manitoba Philosophy Students Association 2025
+                </p>
+            </div>
+        </div>
+    )
+}
